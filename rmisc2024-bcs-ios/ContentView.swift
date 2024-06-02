5cb437a87823d706a19bfdecacd1891e0eb5d30e
Andrew Hoog <ahoog42@gmail.com>
2024-06-02 08:35:02 -0500

    initial commit

---
+++ rmisc2024-bcs-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rmisc2024-bcs-ios
+//
+//  Created by Hiro Protagonist on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rmisc2024-bcs-ios/rmisc2024_bcs_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rmisc2024_bcs_iosApp.swift
+//  rmisc2024-bcs-ios
+//
+//  Created by Hiro Protagonist on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct rmisc2024_bcs_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
