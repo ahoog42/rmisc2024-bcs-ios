9c07fabff8be3fccc42aff28025fe61b13c1388e
Andrew Hoog <ahoog42@gmail.com>
2024-06-06 14:52:26 -0500

    update view names to include View

--- rmisc2024-bcs-ios/ContentView.swift
+++ rmisc2024-bcs-ios/ContentView.swift
@@ -7,17 +7,35 @@
 
 import SwiftUI
 
+// this simple application will have two views
+// the home view will display the most recent 10-K Cybersecurity filings
+// in a list view which will link out to the Board Cybersecurity post
+// there will also be an Account view where the user can login to their account
+// and receive a JWT token
+// the application will follow the MVVM design pattern
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, RSISC 2024!")
+        TabView {
+            NavigationView {
+                HomeView()
+            }
+            .tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            
+            NavigationView {
+                AccountView()
+            }
+            .tabItem {
+                Image(systemName: "person")
+                Text("Account")
+            }
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
