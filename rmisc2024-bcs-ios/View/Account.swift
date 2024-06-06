6701c88421de394666f58075051eec6a977b12b6
Andrew Hoog <ahoog42@gmail.com>
2024-06-06 10:33:10 -0500

    add new folders and 2 blanks views using Xcode

---
+++ rmisc2024-bcs-ios/View/Account.swift
@@ -0,0 +1,18 @@
+//
+//  Account.swift
+//  rmisc2024-bcs-ios
+//
+//  Created by Hiro Protagonist on 6/6/24.
+//
+
+import SwiftUI
+
+struct Account: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Account()
+}
---
+++ rmisc2024-bcs-ios/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  rmisc2024-bcs-ios
+//
+//  Created by Hiro Protagonist on 6/6/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
