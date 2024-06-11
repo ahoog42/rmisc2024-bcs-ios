3ba43838b19fab8789bd3de2af9cced86f1a8568
Andrew Hoog <ahoog42@gmail.com>
2024-06-11 17:25:32 -0500

    create struct for data returned by api

--- rmisc2024-bcs-ios/Model/tenk.swift
+++ rmisc2024-bcs-ios/Model/tenk.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+// create a Codable struct for a 10-K filing object
+// the struct should have the following properties:
+// 1. id: Int
+// 2. filing_id: String
+// 3. company_id: String
+// 4. url: String
+// 5. items_json: String
+// 6. company_name: String
+// 7. filed_at: String
+struct TenK: Codable, Identifiable {
+    var id: Int
+    var filing_id: String
+    var company_id: String
+    var url: String
+    var items_json: String
+    var company_name: String
+    var filed_at: String
+}
